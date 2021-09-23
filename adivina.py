93c2eb58f4f62cb8cb5a6d799538a1f0f6bf5e54
Mauricio Jiménez <34658696+maupz@users.noreply.github.com>
2021-09-22 22:54:55 -0600

    First python script added.

---
+++ adivina.py
@@ -0,0 +1,10 @@
+# -*- coding utf-8 -*-
+
+winner_number = 24
+
+chosen_number = input("Adivina que numero estoy pensando del 1 al 24\n")
+
+if winner_number == chosen_number:
+    print("Haz adivinado")
+else :
+    print("No haz tenido suerte")
